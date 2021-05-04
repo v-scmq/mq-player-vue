6cf339fd091b747921519fcc8d94ecd154eb1664
scmq <1715499885@qq.com>
2021-05-04 15:33:19 +0800

    修正主进程部分逻辑

--- src/main/main.js
+++ src/main/main.js
@@ -2,13 +2,23 @@ import {app, protocol, Tray, Menu, BrowserWindow, ipcMain, net} from 'electron';
 import {createProtocol} from 'vue-cli-plugin-electron-builder/lib';
 import path from 'path';
 
-let iconPath = process.env.NODE_ENV === 'production' ?
-    path.resolve(__dirname, 'icon') : 'public/icon';
+
+/****************************************************************
+ *  应用程序就绪之前,获取环境配置以及注册自定义协议和设置程序资源路径 *
+ ****************************************************************/
+
+// 检查是否是window平台
+let isWindows = process.platform === 'win32';
+// 检查是否是生产模式
+let isProduction = process.env.NODE_ENV === 'production';
+// 获取图标路径
+let iconPath = isProduction ? path.resolve(__dirname, 'icon') : 'public/icon';
 
 // 方案必须在应用程序准备好之前注册
-protocol.registerSchemesAsPrivileged([
-    {scheme: 'app', privileges: {secure: true, standard: true}}
-])
+if (isProduction) {
+    let scheme = {scheme: 'app', privileges: {secure: true, standard: true}};
+    protocol.registerSchemesAsPrivileged([scheme]);
+}
 
 let appPath = app.getAppPath();
 app.setPath('logs', `${appPath}/logs`);
@@ -17,6 +27,11 @@ app.setPath('cache', `${appPath}/cache`);
 app.setPath('appData', `${appPath}/appData`);
 app.setPath('userData', `${appPath}/userData`);
 app.setPath('crashDumps', `${appPath}/crashDumps`);
+
+
+/****************************************************************
+ *   监听应用程序就绪事件,就绪后后创建浏览器窗口和注册自定义的协议等  *
+ ****************************************************************/
 
 // 主进程浏览器窗口, 系统托盘
 let mainWindow = null, tray = null;
@@ -36,88 +51,76 @@ app.on('ready', () => {
         frame: false,
         backgroundColor: 'rgb(236, 236, 236)',
         webPreferences: {
-            nodeIntegration: true,     // 开启NodeJS集成
-            enableRemoteModule: false, // 关闭远程模块,使用ipcMain和ipcRender代替
-            zoomFactor: 0.8,           // 设置浏览器窗口为80%的默认缩放比例
-            webSecurity: true,         // 默认为true,设置为false将关闭web安全策略(如同源策略)
-            contextIsolation: false    // 关闭上下文隔离限制(从版本12.0开始默认为true)
+            nodeIntegration: true,          // 开启NodeJS集成
+            contextIsolation: false,        // 关闭上下文隔离限制(从版本12.0开始默认为true)
+            webSecurity: true,              // 默认为true,设置为false将关闭web安全策略(如同源策略)
+            // enableRemoteModule: false,   // 关闭远程模块,(最佳方案:使用ipcMain和ipcRender代替)
+            // zoomFactor: 0.8,             // 设置浏览器窗口为80%的默认缩放比例(窗口显示后设置,否则很容易失效)
         }
     });
-
-    // 窗口已就绪,此时可以显示
-    let readyShow = () => {
-        createTray();              // 创建托盘
-        mainWindow.show();         // 显示窗口
-        setWindowThumbBarButton(); // 设置任务栏缩略图
-    };
 
     // 若在生产环境
     if (process.env.WEBPACK_DEV_SERVER_URL) {
         // 加载开发模式服务器的URL
-        mainWindow.loadURL(process.env.WEBPACK_DEV_SERVER_URL).then(readyShow);
+        mainWindow.loadURL(process.env.WEBPACK_DEV_SERVER_URL).then(readyToShow);
         mainWindow.webContents.openDevTools();
     } else {
         createProtocol('app');
         // 从自定义协议加载首页html文件
-        mainWindow.loadURL('app://./index.html').then(readyShow);
+        mainWindow.loadURL('app://./index.html').then(readyToShow);
     }
 
     // 当浏览器窗口关闭时,解除mainWindow引用指向
     mainWindow.once('closed', () => mainWindow = null);
     // 当浏览器窗口最大化时,发送窗口最大化消息到渲染进程
-    mainWindow.on('maximize', () => mainWindow.webContents.send('maximized'));
+    mainWindow.on('maximize', async () => mainWindow.webContents.send('maximized'));
     // 当浏览器窗口还原时,发送窗口还原消息到渲染进程
-    mainWindow.on('unmaximize', () => mainWindow.webContents.send('restored'));
+    mainWindow.on('unmaximize', async () => mainWindow.webContents.send('restored'));
 
     // 在渲染进程中,请求最小化浏览器窗口
-    ipcMain.on('window-minimize', () => mainWindow.minimize());
+    ipcMain.handle('window-minimize', async () => mainWindow.minimize());
 
     // 在渲染进程中,请求最大化或还原浏览器窗口
-    ipcMain.on('window-maximize-restore', () =>
+    ipcMain.handle('window-maximize-restore', async () =>
         mainWindow.isMaximized() ? mainWindow.restore() : mainWindow.maximize());
 
     // 在渲染进程中,请求获取窗口状态.在主进程中返回状态到渲染进程
-    ipcMain.handle('request-window-state', () => mainWindow.isMaximized());
+    ipcMain.handle('request-window-state', async () => mainWindow.isMaximized());
 
     // 在渲染进程中,请求主进程清除浏览记录
-    ipcMain.handle('request-clear-history', () => mainWindow.webContents.clearHistory());
+    ipcMain.handle('request-clear-history', async () => mainWindow.webContents.clearHistory());
 
     // 在渲染进程中,请求主进程发起网络请求,主进程将响应信息返回到渲染进程
     ipcMain.handle('net-request', handleNetRequest);
 });
 
 // 当所有窗口关闭后结束进程
-app.on('window-all-closed', () => process.platform !== 'darwin' ? app.quit() : null);
+app.on('window-all-closed', () => app.quit());
 
 // 在macOS上，当单击dock图标并且没有其他窗口打开时，通常会在应用程序中重新创建一个窗口。
 // app.on('activate', () => BrowserWindow.getAllWindows() ? createWindow() : null);
 
 // 应请求以开发模式从父进程中干净地退出。
-if (process.env.NODE_ENV !== 'production') {
-    if (process.platform === 'win32') {
+if (!isProduction) {
+    if (isWindows) {
         process.on('message', data => data === 'graceful-exit' ? app.quit() : null)
     } else {
         process.on('SIGTERM', () => app.quit());
     }
 }
 
-const setWindowThumbBarButton = () => {
-    let prevIcon = `${iconPath}/prev.png`, nextIcon = `${iconPath}/next.png`;
-    let playIcon = `${iconPath}/play.png`, pauseIcon = `${iconPath}/pause.png`;
+/** 窗口已就绪,此时可以显示 */
+let readyToShow = () => {
+    // 显示窗口(将readyToShow方法设置为undefined,以便释放方法对象)
+    readyToShow = mainWindow.show();
+    // 若不是windows平台,不创建系统托盘和设置任务栏缩略图
+    if (!isWindows) {
+        return
+    }
 
-    let playing = false;
+    // 设置浏览器窗口为80%的默认缩放比例
+    mainWindow.webContents.setZoomFactor(0.8);
 
-    /** @type {[{tooltip: string, icon: string|NativeImage, click: (function(): void)}]} */
-    let buttons = [
-        {tooltip: '上一曲', icon: prevIcon, click: () => console.info("prev...")},
-        {tooltip: playing ? '暂停' : '播放', icon: playing ? pauseIcon : playIcon, click: () => console.info("play...")},
-        {tooltip: '下一曲', icon: nextIcon, click: () => console.info("next...")}
-    ];
-    mainWindow.setThumbarButtons(buttons);
-    // mainWindow.setThumbnailClip({x: 0, y: 0, width: 1920, height: 1080});
-}
-
-const createTray = () => {
     // 注意tray需要声明为全局变量,否则会被自动回收对象,导致自动销毁系统托盘
     tray = new Tray(`${iconPath}/tray.ico`);
     let contextMenu = Menu.buildFromTemplate([
@@ -133,6 +136,24 @@ const createTray = () => {
         console.info("event = ", keyEvent);
         console.info("rect = ", rectangle);
     });
+
+    // windows平台下,设置任务栏缩略图
+    let prevIcon = `${iconPath}/prev.png`, nextIcon = `${iconPath}/next.png`;
+    let playIcon = `${iconPath}/play.png`, pauseIcon = `${iconPath}/pause.png`;
+    let playing = false;
+
+    /** @type {[{tooltip: string, icon: string|NativeImage, click: (function(): void)}]} */
+    let buttons = [
+        {tooltip: '上一曲', icon: prevIcon, click: () => console.info("prev...")},
+        {
+            tooltip: playing ? '暂停' : '播放',
+            icon: playing ? pauseIcon : playIcon,
+            click: () => console.info("play...")
+        },
+        {tooltip: '下一曲', icon: nextIcon, click: () => console.info("next...")}
+    ];
+    mainWindow.setThumbarButtons(buttons);
+    // mainWindow.setThumbnailClip({x: 0, y: 0, width: 1920, height: 1080});
 };
 
 /**
