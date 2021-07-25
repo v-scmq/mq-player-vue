41eb09ff75f8c61e165bed8844e04d17806a6f67
SCMQ <1715499885@qq.com>
2021-07-26 00:00:30 +0800

    改善部分主进程部分对于网络层API的封装; 新增打开模态框功能，以获取网站cookie; 新增移除指定URL下的所有cookie功能;

--- src/main/main.js
+++ src/main/main.js
@@ -1,4 +1,4 @@
-import {app, protocol, Tray, Menu, BrowserWindow, ipcMain, net} from 'electron';
+import {app, BrowserWindow, ipcMain, Menu, net, protocol, shell, Tray} from 'electron';
 import {createProtocol} from 'vue-cli-plugin-electron-builder/lib';
 import path from 'path';
 
@@ -33,8 +33,8 @@ app.setPath('crashDumps', `${appPath}/crashDumps`);
  *   监听应用程序就绪事件,就绪后后创建浏览器窗口和注册自定义的协议等  *
  ****************************************************************/
 
-// 主进程浏览器窗口, 系统托盘
-let mainWindow = null, tray = null;
+// 主进程浏览器窗口, 系统托盘, 模态框内容视图(BrowserView)
+let mainWindow = null, tray = null, modal = null;
 
 // 当Electron完成初始化并准备创建浏览器窗口时，将调用此方法 有些API只能在事件发生后使用
 app.on('ready', () => {
@@ -85,13 +85,17 @@ app.on('ready', () => {
         mainWindow.isMaximized() ? mainWindow.restore() : mainWindow.maximize());
 
     // 在渲染进程中,请求获取窗口状态.在主进程中返回状态到渲染进程
-    ipcMain.handle('request-window-state', async () => mainWindow.isMaximized());
+    ipcMain.handle('get-window-state', async () => mainWindow.isMaximized());
 
     // 在渲染进程中,请求主进程清除浏览记录
-    ipcMain.handle('request-clear-history', async () => mainWindow.webContents.clearHistory());
+    ipcMain.handle('clear-history', async () => mainWindow.webContents.clearHistory());
 
     // 在渲染进程中,请求主进程发起网络请求,主进程将响应信息返回到渲染进程
     ipcMain.handle('net-request', handleNetRequest);
+    // 在渲染进程中,请求主进程打开浏览器视图,主进程将Cookie信息返回到渲染进程
+    ipcMain.handle('open-modal', handleOpenModal);
+    // 在渲染进程中,请求主进程删除指定URL下的所有Cookie信息
+    ipcMain.handle('remove-all-cookie', handleRemoveAllCookie);
 });
 
 // 当所有窗口关闭后结束进程
@@ -159,8 +163,8 @@ let readyToShow = () => {
 /**
  * 处理来自渲染进程准备发起的网络请求,最后将响应信息返回到渲染进程
  * @param event {IpcMainInvokeEvent} 渲染进程 => 主进程被调用事件
- * @param options {ClientRequestConstructorOptions} 从渲染进程传递过来的参数
- *  @return {Promise}
+ * @param options {ClientRequestConstructorOptions | Object} 从渲染进程传递过来的参数
+ * @return {Promise<{data:Buffer, headers, statusCode, statusMessage}>} 异步Promise对象
  */
 const handleNetRequest = (event, options) => new Promise(resolve => {
     let request = net.request(options), headers = options['headers'];
@@ -174,24 +178,45 @@ const handleNetRequest = (event, options) => new Promise(resolve => {
         // 返回到渲染进程的数据
         const res = {
             // 响应数据
-            data: '',
+            data: [],
             // 响应头信息
             headers: response.headers,
             // 状态码
             statusCode: response.statusCode,
             // 状态信息
-            statusMessage: response.statusMessage,
+            statusMessage: response.statusMessage
         };
 
-        // 改变Promise对象状态:由pending(进行中)转变为resolved(已成功)
-        const resolved = () => resolve(JSON.stringify(res));
-
         // 数据得到响应时,拼接响应数据
-        response.on('data', chunk => res.data += chunk.toString());
+        response.on('data', chunk => res.data.push(chunk));
+
+        // 改变Promise对象状态:由pending(进行中)转变为resolved(已成功)
         // 数据响应完成时,promise对象状态变为resolved以将数据返回
-        response.on('end', resolved);
+        response.on('end', () => {
+            res.data = Buffer.concat(res.data);
+            let {responseType: type, RESPONSE_TYPE: types} = options;
+
+            // 若配置选项提供返回类型是字节类型,那么返回的将是Buffer(Uint8Array子类)类型
+            if (type === types.BYTE) {
+                return resolve(res);
+            }
+
+            // 若配置选项提供响应内容为文本,则直接返回字符串
+            res.data = res.data.toString();
+            if (type === types.TEXT) {
+                return resolve(res);
+            }
+
+            // 配置选项提供响应类型为JSON(默认),那么返回反序列化后的JSON格式的对象
+            try {
+                res.data = JSON.parse(res.data);
+            } catch (error) {
+                res.data = {};
+            }
+            return resolve(res);
+        });
         // 数据响应失败
-        response.on('error', resolved);
+        response.on('error', () => resolve(res));
     });
 
     // 当前网络请求发生错误时
@@ -212,5 +237,82 @@ const handleNetRequest = (event, options) => new Promise(resolve => {
     // 当网络请求完成后
     request.on('finish', () => console.info('请求完成！'));
     request.end();
+});
+
+/**
+ * 处理来自渲染进程请求打开模态框,最后将页面的Cookie信息返回到渲染进程
+ * @param event {IpcMainInvokeEvent} 渲染进程 => 主进程被调用事件
+ * @param options {String | Object} 从渲染进程传递过来的参数
+ * @return {Promise<String>} 异步Promise对象
+ */
+const handleOpenModal = (event, options) => new Promise(resolve => {
+    /** @type {{url:String, indexURL:String, width:Number, height:Number, preloadName:String}} */
+    options = (typeof options) === 'string' ? JSON.parse(options) : options;
+
+    // 若指定了预加载文件名(不包含扩展名),则获取需要执行预加载文件的全路径
+    let preload = options.preloadName ? `${__dirname}${isProduction ? '' : '/../public'}/${options.preloadName}.js` : null;
+
+    // 若模态框未初始化,则先初始化(窗口圆角效果, 需要设置窗口透明且不能打开开发者工具,否则无效果)
+    modal = modal || new BrowserWindow({
+        width: options.width, height: options.height, parent: mainWindow,
+        modal: true, show: false, resizable: false, frame: false,
+        transparent: true, webPreferences: {webSecurity: false, preload}
+    });
+
+    // 加载指定的URL
+    modal.loadURL(options.url).then();
+    // 设置页面缩放比例为80%
+    modal.once('ready-to-show', () => {
+        modal.show();
+        modal.webContents.setZoomFactor(0.8);
+    });
+
+    // 当页面开始导航(页面跳转)
+    modal.webContents.on('will-navigate', (event, url) => {
+        if (options.indexURL === url) {
+            let _resolve = resolve, url = options.indexURL, cookies = modal.webContents.session.cookies;
+            cookies.get({url}).then(cookieArray => {
+                /** @type {{url, name, value, domain, path, secure, httpOnly, expirationDate, sameSite}  | null} */
+                let detail = null;
+                cookieArray.forEach(cookie => {
+                    detail = {...cookie, url, sameSite: 'no_restriction', secure: true};
+                    cookies.set(detail).then();
+                });
+                _resolve(JSON.stringify(cookieArray));
+            });
+            modal.webContents.closeDevTools();
+            modal.close(resolve = null);
+        }
+    });
+
+    // 当模态框准备关闭时
+    modal.once('close', () => {
+        if (modal) {
+            modal.webContents.closeDevTools();
+            // destroy()方法返回值是void, modal将断开引用
+            modal = modal.destroy();
+        }
+        resolve ? resolve(null) : null;
+    });
+
+    // 设置窗口打开处理器
+    modal.webContents.setWindowOpenHandler(details => {
+        // 使用系统默认浏览器打开模态框内的连接
+        shell.openExternal(details.url).then();
+        // 拒绝新窗口打开模态框内的所有连接
+        return {action: 'deny'};
+    });
+});
+
+/**
+ * 处理来自渲染进程请求删除指定URL下的cookie信息
+ * @param event {IpcMainInvokeEvent} 渲染进程 => 主进程被调用事件
+ * @param url {String} cookie对应的URL
+ * @return {Promise<String>} 异步Promise对象
+ */
+const handleRemoveAllCookie = (event, url) => new Promise(resolve => {
+    let cookies = mainWindow.webContents.session.cookies;
+    cookies.get({url}).then(cookieArray => cookieArray.forEach(cookie => cookies.remove(url, cookie.name)));
+    resolve(null);
 });
 
