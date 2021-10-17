4b2abce4808284264ebec1496ad012f0f12c5ee9
scmq <1715499885@qq.com>
2021-10-17 22:46:21 +0800

    调整配置项

--- vue.config.js
+++ vue.config.js
@@ -86,6 +86,9 @@ module.exports = {
         // vue-cli-plugin-electron-builder配置
         electronBuilder: {
             preload: 'src/main/preload.js',
+            outputDir: 'dist_electron',
+            mainProcessFile: 'src/main/main.js',
+            mainProcessWatch: ['src/main'],
             builderOptions: {
                 win: {
                     // icon: 'src/assets/icons/icon.ico',
@@ -94,7 +97,7 @@ module.exports = {
                         // 打包成一个独立的 exe 安装程序
                         target: 'nsis',
                         // 这个意思是打出来32 bit + 64 bit的包，但是要注意：这样打包出来的安装包体积比较大，所以建议直接打32的安装包。
-                        'arch': ['x64'] // 'ia32'
+                        arch: ['x64'] // 'ia32'
                     }]
                 },
                 dmg: {
@@ -137,10 +140,7 @@ module.exports = {
                     args[0]['IS_ELECTRON'] = true
                     return args
                 })
-            },
-            outputDir: 'dist_electron',
-            mainProcessFile: 'src/main/main.js',
-            mainProcessWatch: ['src/main']
+            }
         }
     }
 }
