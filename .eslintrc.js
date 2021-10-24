1f3ad07c2971f87a1ca89f0d2866f87b9cf9ad24
scmq <1715499885@qq.com>
2021-10-24 20:21:19 +0800

    提交远程在本地仓库中缺少的文件

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    root: true,
+    env: {node: true, es6: true},
+    extends: ['plugin:vue/vue3-essential', 'eslint:recommended'],
+    parserOptions: {ecmaVersion: 2020},
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    }
+}
