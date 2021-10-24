4e8517f787e22f9afe8a8ac68ba4d240c501a164
scmq <1715499885@qq.com>
2021-10-24 23:37:27 +0800

    移除下载页面内容

--- src/renderer/views/Download.vue
+++ src/renderer/views/Download.vue
@@ -1,27 +1,12 @@
 <template>
-  <div class="v-row">
-    <text-field v-model="usedTime" style="margin: 0 16px;"/>
-    <pagination :dataCount="1000" v-model:current="current"/>
-  </div>
+  下载管理
 </template>
 
 <script>
-import Pagination from '../components/Pagination.vue';
 export default {
-  components: { Pagination },
   name: "Download",
-  data: () => ({usedTime: null, current:1}),
-  created() {
-
-  },
-  watch:{
-    current(newValue){
-      console.info(newValue);
-    }
-  }
 }
 </script>
 
 <style scoped>
-
 </style>
