e44dcf017d0fcaff88ba9b41c1ba13bdbc0773bf
scmq <1715499885@qq.com>
2021-10-25 18:30:49 +0800

    修改完善分页组件

--- src/renderer/views/doc/PaginationDemo.vue
+++ src/renderer/views/doc/PaginationDemo.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <pagination v-model:current="page.current" :data-count="page.total" :size="page.size"/>
+  <div class="v-column">
+    <pagination v-model:currentPage="page.current" :total="page.total" :pageSize="page.size" style="margin: 20px 0;"/>
+
+    <el-pagination layout="prev, pager, next" :total="100" :pagerCount="7"></el-pagination>
   </div>
 </template>
 
