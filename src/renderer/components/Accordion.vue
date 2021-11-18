ec643f951b2923487cd789b60bd78a1b4561a1ed
scmq <1715499885@qq.com>
2021-11-19 00:00:35 +0800

    优化手风琴组件

--- src/renderer/components/Accordion.vue
+++ src/renderer/components/Accordion.vue
@@ -1,78 +1,72 @@
 <template>
   <div class='accordion v-column' ref='el' :style="{'--accordion-scroll-wrapper-height':`${scrollWrapperHeight}px`}">
-    <div class='list-view v-column' v-for='(data,index) in list' :key='index' @click='onClick($event,data,index)'>
+    <div class='list-view v-column' v-for='(data, groupIndex) in list' @click='onClick($event, data, groupIndex)'
+         :class='{expand: expandedIndex === groupIndex}' :key='groupIndex'>
+
       <div class='v-row titled-pane'>{{ data.title }}</div>
+
       <div class='v-column scroll-wrapper'>
-        <div class='item' v-for='(item,index) in data.items' :key='index' :data-index='index'>{{ item.name }}</div>
+        <div class='item' v-for='(item, index) in data.items' :key='index' :data-index='index'
+             :class='{selected: selectedId === `${groupIndex}-${index}`}'>{{ item.name }}
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import {nextTick, onBeforeUnmount, onMounted, ref, watch, getCurrentInstance} from 'vue';
+import {onBeforeUnmount, onMounted, ref, getCurrentInstance} from 'vue';
+
+/**
+ * @typedef {Object} DataItem 手风琴子列表项信息
+ *
+ * @property {string | number | undefined} id 数据id
+ * @property {string} name 名称
+ */
+
+/**
+ * @typedef {Object} DataItemGroup 手风琴主列表项信息
+ *
+ * @property {string | number | undefined} id 分组id
+ * @property {string} title 分组标题
+ * @property {DataItem[]} items 子列表项数据信息
+ */
 
 export default {
   name: 'Accordion',
-  props: {list: {default: []}},
+  props: {list: {default: /** @type {DataItemGroup[]} */[]}},
   emits: ['change'],
 
-  setup(props, context) {
+  setup(props, {emit}) {
+    // 组件高度
     const scrollWrapperHeight = ref(0);
+    // 主列表项已展开的索引
+    const expandedIndex = ref(0);
+    // 子列表项被选中的索引(分类组索引-子分类索引)
+    const selectedId = ref('0-0');
 
-    let selectedItem = null;
-
-    let /** @type {HTMLElement} */ el, /** @type {ResizeObserver} */ resizeObserver;
-
-    /** 当没有任何展开的列表项时,展开一个默认的列表项 */
-    const expandDefault = () => {
-      let nodes = el.querySelectorAll('.list-view');
-      // 若没有任何列表项时,则重置已选定的记录
-      if (nodes.length < 1) {
-        selectedItem = null;
-        return;
-      }
-
-      // 检测列表项是否至少有一项被展开
-      for (let node of nodes) {
-        if (node.classList.contains('expand')) {
-          return;
-        }
-      }
-
-      // 默认展开列表项的第一个
-      nodes[0].classList.toggle('expand');
-      // 获取第一个列表项的第一个子列表项
-      let node = nodes[0].querySelector('.scroll-wrapper .item:first-child');
-      // 若存在子列表项
-      if (node) {
-        // 记录第一个列表项的第一个子列表项为所选记录
-        selectedItem = props.list[0].items[0];
-        // 强制选中第一个列表项的第一个子列表项
-        node.classList.toggle('selected', true);
-      }
-    };
+    /** @type {HTMLElement} */
+    let el;
+    /** @type {ResizeObserver} */
+    let resizeObserver;
 
     onMounted(() => {
       el = getCurrentInstance().refs.el;
 
       // 当根元素宽高发生变化时,回调此方法以计算滚动盒子内部高度
       resizeObserver = new ResizeObserver(([entry]) => {
+        // 获取组件根节点 和 组件根节点的高度
         let node = entry.target, height = node.offsetHeight;
+        // 获取所有标题层子节点
         let nodes = node.querySelectorAll('.list-view .titled-pane');
+        // 滚动高度 = 根节点高度 - 所有标题层子节点高度
         nodes.forEach(node => height -= node.offsetHeight);
+        // 设置滚动高度
         scrollWrapperHeight.value = height;
       });
 
+      // 观察根节点大小变化
       resizeObserver.observe(el);
-
-      if (props.list && props.list.length) {
-        let items = props.list[0].items;
-        if (items && items.length) {
-          selectedItem = items[0];
-        }
-        nextTick(() => expandDefault())
-      }
     });
 
     onBeforeUnmount(() => {
@@ -83,43 +77,30 @@ export default {
       }
     });
 
-    /** 当数据发生变化时 */
-    watch(props.list, () => nextTick(expandDefault));
-
     return {
-      scrollWrapperHeight,
+      scrollWrapperHeight, expandedIndex, selectedId,
       /**
        * ListView被点击时触发
        * @param {MouseEvent} event               鼠标事件
-       * @param {{name:String,items:Array}} data 鼠标所击对应的数据项
-       * @param {Number} index                   鼠标所击对应的数据项索引
+       * @param {DataItemGroup} data 鼠标所击对应的数据项
+       * @param {number} index                   鼠标所击对应的数据项索引
        */
       onClick: (event, data, index) => {
         event.stopPropagation();
-        let nodes = el.querySelectorAll('.list-view');
-        let node = event.target, classList = node.classList;
-        let parent = classList.contains('list-view') ? node : node.parentNode;
+
+        const node = event.target, classList = node.classList;
 
         if (classList.contains('item')) {
-          // 获取所有选中的ListView的子项
-          nodes = el.querySelectorAll('.list-view .item.selected');
-          // 移除选中class标记
-          nodes.forEach(node => node.classList.remove('selected'));
-          // 强制为当前所点击ListView子项添加选中class标记
-          classList.toggle('selected', true);
-          let current = data.items[node.getAttribute('data-index')];
-          if (selectedItem.value !== current) {
-            context.emit('change', selectedItem = current, data, index);
+          const itemIndex = node.getAttribute('data-index') ^ 0;
+          const newId = `${index}-${itemIndex}`;
+          if (selectedId.value !== newId) {
+            selectedId.value = newId;
+            emit('change', data.items[itemIndex], data, index);
           }
-          return;
-        }
 
-        for (let item of nodes) {
-          if (parent === item) {
-            item.classList.toggle('expand');
-          } else {
-            item.classList.toggle('expand', false);
-          }
+        } else {
+          // 若当前分类组已被展开,那么折叠它, 否则展开它
+          expandedIndex.value = expandedIndex.value === index ? -1 : index;
         }
       }
     };
