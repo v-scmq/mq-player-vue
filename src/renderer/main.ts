84fa6d5ef4a6bcd4e1789da3b4d624c7236d3d54
scmq <1715499885@qq.com>
2021-12-12 22:04:54 +0800

    分离组件样式并独立导入, 优化部分页面样式

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -3,6 +3,7 @@ import app from './App.vue';
 import router from './router';
 
 import control from './components';
+import '../assets/css/base.css';
 import '../assets/css/style.css';
 
 createApp(app).use(control).use(router).mount('#app');
