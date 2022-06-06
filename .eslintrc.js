f9d873ea75bc81d0e83e27b48e582192cb470c39
scmq <1715499885@qq.com>
2022-06-06 08:33:45 +0800

    修改Eslint配置,忽略组件命名检验

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,8 @@ module.exports = {
 
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'vue/multi-word-component-names': 'off' // 关闭Vue组件命名检验
     },
 
     extends: [
