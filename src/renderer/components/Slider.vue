b434a791e66330a87f7117ab247be5d241067237
scmq <1715499885@qq.com>
2022-06-06 08:32:55 +0800

    优化Slider组件

--- src/renderer/components/Slider.vue
+++ src/renderer/components/Slider.vue
@@ -1,14 +1,15 @@
 <template>
   <div class='slider' ref='el' :class='{vertical}' @click='onSliderClicked'>
     <div class='track'>
-      <div class='buffer' v-if='buffering >= 0' :style='{width:`${buffering * 100}%`}'/>
 
       <template v-if='vertical'>
+        <div class='buffer' v-if="bufferNodeVisible" :style='{height:`${buffering * 100}%`}'/>
         <div class='filler' :style='{height: `${value}%`}'/>
         <div class='thumb' ref='thumb' :style='{top: `${100 - value}%`}' @pointerdown='onDragStart'/>
       </template>
 
       <template v-else>
+        <div class='buffer' v-if="bufferNodeVisible" :style='{width:`${buffering * 100}%`}'/>
         <div class='filler' :style='{width: `${value}%`}'/>
         <div class='thumb' ref='thumb' :style='{left: `${value}%`}' @pointerdown='onDragStart'/>
       </template>
@@ -30,10 +31,10 @@
  *
  * @author SCMQ
  * @date 2020-12-13
- * @update 2021-09-02
+ * @update 2021-09-02、2022-06-03
  */
 
-import {ref, watch, computed, defineComponent} from 'vue';
+import {computed, ref, watch, defineComponent} from 'vue';
 
 export default defineComponent({
   name: 'Slider',
@@ -65,37 +66,8 @@ export default defineComponent({
     // 进度值
     const value = computed(() => props.modelValue * 100);
 
-    /**
-     * 开始拖动(指针设备在滑块上按下)时触发, 此时并还未开始拖动,仅仅是准备好拖动.
-     * 需要注意检测中断拖动不能依靠滑块(div)元素本身,需要借助document对象 或 window对象
-     * 因为元素本身在鼠标移动后,所监听的鼠标释放事件并不会触发,但是document或window一定会触发
-     *
-     * @param {PointerEvent} event 指针事件
-     */
-    const onDragStart = (event: PointerEvent) => {
-      event.preventDefault();
-
-      const value = props.vertical;
-      offsetX = value ? event.clientY : event.clientX;
-      offsetLeft = value ? thumb.value.offsetTop : thumb.value.offsetLeft;
-      document.onpointermove = onDragging;
-      document.onpointerup = onDragEnd;
-    };
-
-    /**
-     * 鼠标停止拖动(鼠标在页面开始按下后,然后释放)时触发.
-     * 此时只会提交change事件,并且传出的第2个参数boolean值true以方便侦测滑块拖动结束
-     */
-    const onDragEnd = () => {
-      // 在拖动处理的onDragging方法中, 若确实发生值的变化,dragged值才会为true
-      if (dragged) {
-        // 触发一次改变事件,并传出boolean值true作为拖动结束的标记
-        emit('change', props.modelValue, true);
-      }
-
-      dragged = offsetX = offsetLeft = null as any;
-      document.onpointermove = document.onpointerup = null;
-    };
+    // 发出change事件,以便侦测数据值改变
+    watch(() => props.modelValue, newValue => emit('change', newValue));
 
     /**
      * 滑块被拖动时触发.在此过程中,只会提交input事件以修改value属性值
@@ -121,13 +93,61 @@ export default defineComponent({
     };
 
     /**
-     * 滑动条被点击时触发,随着先后触发input事件和change事件
-     * 触发change事件并传出第二个参数值true 以方便侦测滑动条的值是人为改变
-     *
-     * @param {PointerEvent} e 指针事件
+     * 鼠标停止拖动(鼠标在页面开始按下后,然后释放)时触发.
+     * 此时只会提交change事件,并且传出的第2个参数boolean值true以方便侦测滑块拖动结束
      */
-    const onSliderClicked = (e: PointerEvent) => {
-      if (e.target === el.value) {
+    const onDragEnd = () => {
+      // 在拖动处理的onDragging方法中, 若确实发生值的变化,dragged值才会为true
+      if (dragged) {
+        // 触发一次改变事件,并传出boolean值true作为拖动结束的标记
+        emit('change', props.modelValue, true);
+      }
+
+      dragged = offsetX = offsetLeft = null as any;
+      document.onpointermove = document.onpointerup = null;
+    };
+
+    return {
+      el, thumb, value,
+
+      bufferNodeVisible: computed(() => {
+        const {buffering} = props;
+        return !!buffering && buffering > 0;
+      }),
+
+      /**
+       * 检测滑块是否正在被拖动
+       * @returns {boolean} 若滑块在被鼠标拖动,则返回true;否则返回false
+       */
+      isNotDragging: () => offsetX == null,
+
+      /**
+       * 开始拖动(指针设备在滑块上按下)时触发, 此时并还未开始拖动,仅仅是准备好拖动.
+       * 需要注意检测中断拖动不能依靠滑块(div)元素本身,需要借助document对象 或 window对象
+       * 因为元素本身在鼠标移动后,所监听的鼠标释放事件并不会触发,但是document或window一定会触发
+       *
+       * @param {PointerEvent} event 指针事件
+       */
+      onDragStart(event: PointerEvent) {
+        event.preventDefault();
+
+        const value = props.vertical;
+        offsetX = value ? event.clientY : event.clientX;
+        offsetLeft = value ? thumb.value.offsetTop : thumb.value.offsetLeft;
+        document.onpointermove = onDragging;
+        document.onpointerup = onDragEnd;
+      },
+
+      /**
+       * 滑动条被点击时触发,随着先后触发input事件和change事件
+       * 触发change事件并传出第二个参数值true 以方便侦测滑动条的值是人为改变
+       *
+       * @param {PointerEvent} e 指针事件
+       */
+      onSliderClicked(e: PointerEvent) {
+        if (e.target === thumb.value) {
+          return;
+        }
 
         let value = props.vertical
             ? e.offsetY / el.value.clientHeight
@@ -141,18 +161,9 @@ export default defineComponent({
           emit('change', value, true);
         }
       }
+
     };
 
-    // 发出change事件,以便侦测数据值改变
-    watch(() => props.modelValue, newValue => emit('change', newValue));
-
-    /**
-     * 检测滑块是否正在被拖动
-     * @returns {boolean} 若滑块在被鼠标拖动,则返回true;否则返回false
-     */
-    const isNotDragging = () => offsetX == null;
-
-    return {el, thumb, value, onSliderClicked, onDragStart, isNotDragging};
   }
 
 });
