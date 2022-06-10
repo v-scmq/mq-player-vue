0e8d826a5da346ae9319f7d3c988094740c3b56d
scmq <1715499885@qq.com>
2022-06-10 16:05:58 +0800

    移除非必需的页面根元素

--- src/renderer/views/doc/SliderDemo.vue
+++ src/renderer/views/doc/SliderDemo.vue
@@ -1,30 +1,28 @@
 <template>
-  <div class="v-column" style="gap: 24px; align-items: center">
-    <slider style="width:100%" v-model="value"></slider>
+  <slider style="" v-model="value"></slider>
 
-    <div class="v-row" style="height: 160px;width: 128px; gap: 40px;justify-content: center">
+  <div class="v-row" style="height: 160px;width: 128px; gap: 40px;justify-content: center">
+    <slider style="height:100%" vertical v-model="value"></slider>
+    <slider style="height:100%" vertical v-model="value"></slider>
+  </div>
+
+  <div class="v-row" style="gap: 20px">
+    <div class="v-column" style="height: 160px;width: 56px;align-items:center">
       <slider style="height:100%" vertical v-model="value"></slider>
-      <slider style="height:100%" vertical v-model="value"></slider>
+      <span>{{ (value * 100).toFixed(0) }}%</span>
     </div>
 
-    <div class="v-row" style="gap: 20px">
-      <div class="v-column" style="height: 160px;width: 56px;align-items:center">
-        <slider style="height:100%" vertical v-model="value"></slider>
-        <span>{{ (value * 100).toFixed(0) }}%</span>
-      </div>
-
-      <div class="v-column" style="height: 160px;align-items: center">
-        <div class="v-row" style="height: 160px;gap:8px;justify-content:center;align-items:stretch">
-          <slider vertical v-model="value"></slider>
-          <div class="v-column" style="justify-content:space-between">
-            <span>2.0</span>
-            <span>1.5</span>
-            <span>1.0</span>
-            <span>0.5</span>
-          </div>
+    <div class="v-column" style="height: 160px;align-items: center">
+      <div class="v-row" style="height: 160px;gap:8px;justify-content:center;align-items:stretch">
+        <slider vertical v-model="value"></slider>
+        <div class="v-column" style="justify-content:space-between">
+          <span>2.0</span>
+          <span>1.5</span>
+          <span>1.0</span>
+          <span>0.5</span>
         </div>
-        <span>{{ (1.5 * value + 0.5).toFixed(1) }}</span>
       </div>
+      <span>{{ (1.5 * value + 0.5).toFixed(1) }}</span>
     </div>
   </div>
 </template>
